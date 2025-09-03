b5ba1b53103015a67506d6ca21549e5c4c3e9138
EmilioJimenezL <141659948+EmilioJimenezL@users.noreply.github.com>
2025-09-03 12:19:42 -0600

    added mux

---
+++ mux.v
@@ -0,0 +1,17 @@
+module mux (
+    input  [3:0] data_in,
+    input  [1:0] sel,
+    output reg out
+);
+
+always @(*) begin
+    case (sel)
+        2'b00: out = data_in[0];
+        2'b01: out = data_in[1];
+        2'b10: out = data_in[2];
+        2'b11: out = data_in[3];
+        default: out = 1'b0; // Optional, for safety
+    endcase
+end
+
+endmodule
