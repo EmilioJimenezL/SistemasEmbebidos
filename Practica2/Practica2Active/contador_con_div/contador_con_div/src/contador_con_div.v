af513dff4b3165cec324a30508a131d81c098e5e
EmilioJimenezL <emilio.jimenezlz@udlap.mx>
2025-09-17 12:58:28 -0600

    modified contador con div

--- Practica2/Practica2Active/contador_con_div/contador_con_div/src/contador_con_div.v
+++ Practica2/Practica2Active/contador_con_div/contador_con_div/src/contador_con_div.v
@@ -38,30 +38,6 @@ end
 
 endmodule
 
-module contador_con_div(
-	input wire clk,
-	input wire rst,
-	input wire en,
-	output wire [3:0] count
-	);
-	
-	wire clk_1hz;
-	
-	freq_div div1( 
-	.clk(clk),
-	.rst(rst),
-	.clk_1hz(clk_1hz)
-	);	   
-	
-	contador_ascendente contador1(	 
-	.clk(clk_1hz),
-	.rst(rst),
-	.en(en),
-	.count(count)
-	); 
-	
-endmodule  
-
 module binary_to_hex_cathode_comun (
     input wire A, B, C, D,
     output wire a, b, c, d, e, f, g
@@ -89,3 +65,62 @@ assign f = (~C & ~D) | (~A & B & ~C) | (A & ~B) | (B & C & ~D) | (A & C);
 assign g = (~A & B & ~C) | (~A & ~B & C) | (A & ~B) | (C & ~D) | (A & D);
 
 endmodule
+
+module binary_to_hex_decoder (
+    input [3:0] binary_in,
+    output reg [6:0] hex_out
+);
+
+always @(*) begin
+    case (binary_in)
+        4'h0: hex_out = 7'b0000001; // 0
+        4'h1: hex_out = 7'b1001111; // 1
+        4'h2: hex_out = 7'b0010010; // 2
+        4'h3: hex_out = 7'b0000110; // 3
+        4'h4: hex_out = 7'b1001100; // 4
+        4'h5: hex_out = 7'b0100100; // 5
+        4'h6: hex_out = 7'b0100000; // 6
+        4'h7: hex_out = 7'b0001111; // 7
+        4'h8: hex_out = 7'b0000000; // 8
+        4'h9: hex_out = 7'b0000100; // 9
+        4'hA: hex_out = 7'b0001000; // A
+        4'hB: hex_out = 7'b1100000; // b
+        4'hC: hex_out = 7'b0110001; // C
+        4'hD: hex_out = 7'b1000010; // d
+        4'hE: hex_out = 7'b0110000; // E
+        4'hF: hex_out = 7'b0111000; // F
+        default: hex_out = 7'b1111111; // Apagado
+    endcase
+end
+
+endmodule	 
+
+module contador_con_div(
+	input wire clk,
+	input wire rst,
+	input wire en,
+	output wire [6:0] seg,
+	output wire [3:0] count
+	);
+	
+	wire clk_1hz;
+	
+	freq_div div1( 
+	.clk(clk),
+	.rst(rst),
+	.clk_1hz(clk_1hz)
+	);	   
+	
+	contador_ascendente contador1(	 
+	.clk(clk_1hz),
+	.rst(rst),
+	.en(en),
+	.count(count)
+	);
+	
+	binary_to_hex_decoder decoder( 
+	.binary_in(count),
+	.hex_out(seg)
+	);
+	
+endmodule  
