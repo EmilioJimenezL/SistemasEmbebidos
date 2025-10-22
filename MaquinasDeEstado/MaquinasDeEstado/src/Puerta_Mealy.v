5cbd9c5f5289ad4b7cd2337702c5bca471b7ab9c
EmilioJimenezL <emilio.jimenezlz@udlap.mx>
2025-10-22 11:22:41 -0600

    corrected code for MaquinasDeEstados, rm Practica4

---
+++ MaquinasDeEstado/MaquinasDeEstado/src/Puerta_Mealy.v
@@ -0,0 +1,139 @@
+module Puerta_Mealy (
+    input clk,         // Reloj principal (50MHz)
+    input rst,         // Reset
+    input sense,       // Sensor de presencia
+    input obs,         // Sensor de obstáculo
+    output reg [1:0] motor, // Salida del motor
+    output reg alarm, // Alarma
+	output reg led_clk
+);
+
+	parameter DIVISOR = 50000000; // 1Hz si clk = 50MHz
+	parameter MAX_SECONDS = 5;	//Amount of seconds to wait to switch the slow clock
+
+    // ========================
+    // Divisor de frecuencia
+    // ========================
+    reg [$clog2(DIVISOR)-1:0] contador = 0;
+    reg clk_fsm = 0;
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            contador <= 0;
+            clk_fsm <= 0;
+        end else begin
+            if (contador == DIVISOR/2 - 1) begin
+                clk_fsm <= ~clk_fsm;
+                contador <= 0;
+            end else begin
+                contador <= contador + 1;
+            end
+        end
+    end
+
+    // Estados
+    parameter [1:0] Cerrado  = 2'b00,
+                    Abierto  = 2'b01,
+                    Cerrando = 2'b10;
+
+    reg [1:0] actual = Cerrado, siguiente;
+    reg [3:0] open_counter = 0; // hasta 10
+    reg waiting = 0;
+
+    // Lógica secuencial
+    always @(posedge clk_fsm or posedge rst) begin
+        if (rst) begin
+            actual <= Cerrado;
+            open_counter <= 0;
+            waiting <= 0;
+        end else begin
+			led_clk <= ~led_clk;
+            actual <= siguiente;
+
+            if (actual == Abierto && sense == 0 && obs == 0) begin
+                if (open_counter < MAX_SECONDS) begin
+                    open_counter <= open_counter + 1;
+                    waiting <= 1;
+                end else begin
+                    waiting <= 0;
+                    open_counter <= 0;
+                end
+            end else begin
+                open_counter <= 0;
+                waiting <= 0;
+            end
+        end
+    end
+
+    // Lógica combinacional
+    always @(*) begin
+        case (actual)
+            Cerrado: begin
+                if (!sense && !obs) begin
+                    siguiente = Cerrado;
+					motor = 2'b00;
+                    alarm = 0;
+                end else if (!sense && obs) begin
+                    siguiente = Cerrado; 
+					motor = 2'b00;
+                    alarm = 0;
+                end else if (sense && !obs) begin
+                    siguiente = Abierto;
+					motor = 2'b01;
+                    alarm = 0;
+                end	else begin
+					siguiente = Abierto;
+					motor = 2'b01;
+                    alarm = 0;
+				end
+            end
+
+            Abierto: begin
+                if (!sense && !obs) begin
+                    siguiente = Cerrando;
+					motor = 2'b10;
+                    alarm = 0;
+                end else if (!sense && obs) begin
+                    siguiente = Abierto; 
+					motor = 2'b00;
+                    alarm = 0;
+                end else if (sense && !obs) begin
+                    siguiente = Abierto;
+					motor = 2'b00;
+                    alarm = 0;
+                end	else begin
+					siguiente = Abierto;
+					motor = 2'b00;
+                    alarm = 0;
+				end
+            end
+
+            Cerrando: begin
+                if (!sense && !obs) begin
+                    siguiente = Cerrado;
+					motor = 2'b10;
+                    alarm = 0;
+                end else if (!sense && obs) begin
+                    siguiente = Abierto; 
+					motor = 2'b01;
+                    alarm = 1;
+                end else if (sense && !obs) begin
+                    siguiente = Cerrado;
+					motor = 2'b10;
+                    alarm = 0;
+                end	else begin
+					siguiente = Abierto;
+					motor = 2'b01;
+                    alarm = 1;
+				end
+            end
+
+            default: begin
+                siguiente = Cerrado;
+                alarm = 0;
+                motor = 2'b00;
+            end
+        endcase
+    end
+
+endmodule	 
---
+++ MaquinasDeEstado/MaquinasDeEstado/src/Puerta_Moore.v
@@ -0,0 +1,159 @@
+module Puerta_Moore (
+    input clk,         // Reloj principal (50MHz)
+    input rst,         // Reset
+    input sense,       // Sensor de presencia
+    input obs,         // Sensor de obstáculo
+    output reg [1:0] motor, // Salida del motor
+    output reg alarm,        // Alarma
+	output reg led_clk
+);
+
+	parameter DIVISOR = 50000000; // 1Hz si clk = 50MHz
+	parameter MAX_SECONDS = 5; //Amount of seconds to wait to switch the slow clock
+
+    // ========================
+    // Divisor de frecuencia
+    // ========================
+    reg [$clog2(DIVISOR)-1:0] contador = 0;
+    reg clk_fsm = 0;
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            contador <= 0;
+            clk_fsm <= 0;
+        end else begin
+            if (contador == DIVISOR/2 - 1) begin
+                clk_fsm <= ~clk_fsm;
+                contador <= 0;
+            end else begin
+                contador <= contador + 1;
+            end
+        end
+    end
+
+    // Estados
+    parameter [2:0] Cerrado  = 3'b000,
+                    Abriendo  = 3'b001,
+                    Abierto = 3'b010,
+					Cerrando = 3'b011,
+					Alarma = 3'b100;
+
+    reg [2:0] actual = Cerrado, siguiente;
+    reg [3:0] open_counter = 0; // hasta 10
+    reg waiting = 0;
+
+    // Lógica secuencial
+    always @(posedge clk_fsm or posedge rst) begin
+        if (rst) begin
+            actual <= Cerrado;
+			siguiente <= Cerrado;
+            open_counter <= 0;
+            waiting <= 0;
+        end else begin
+			led_clk <= ~led_clk;
+            actual <= siguiente;
+
+            if (actual == Abierto && sense == 0 && obs == 0) begin
+                if (open_counter < MAX_SECONDS) begin
+                    open_counter <= open_counter + 1;
+                    waiting <= 1;
+                end else begin
+                    waiting <= 0;
+                    open_counter <= 0;
+                end
+            end else begin
+                open_counter <= 0;
+                waiting <= 0;
+            end
+        end
+    end
+
+    // Lógica combinacional
+    always @(*) begin
+		case (actual)
+			Cerrado: begin
+				motor = 2'b00;
+				alarm = 0;	 
+			end
+			Abriendo: begin
+				motor = 2'b01;
+				alarm = 0;	 
+			end
+			Abierto: begin
+				motor = 2'b00;
+				alarm = 0;	 
+			end			  
+			Cerrando: begin
+				motor = 2'b10;
+				alarm = 0;	 
+			end			  
+			Alarma: begin
+				motor = 2'b00;
+				alarm = 1;	 
+			end
+		endcase
+		
+        case (actual)
+            Cerrado: begin
+                if (!sense && !obs)
+                    siguiente = Cerrado;
+                else if (!sense && obs)
+                    siguiente = Cerrado;
+                else if (sense && !obs)
+					siguiente = Abriendo;
+				else
+					siguiente = Abriendo;
+			end
+			Abriendo: begin
+				if (!sense && !obs)
+                    siguiente = Abierto;
+                else if (!sense && obs)
+                    siguiente = Alarma;
+                else if (sense && !obs)
+					siguiente = Abierto;
+				else
+					siguiente = Alarma;
+			end		
+
+            Abierto: begin
+                if (!sense && !obs)
+                    siguiente = Cerrando;
+                else if (!sense && obs)
+                    siguiente = Abierto;
+                else if (sense && !obs)
+					siguiente = Abierto;
+				else
+					siguiente = Abierto;
+            end
+
+            Cerrando: begin
+                if (!sense && !obs)
+                    siguiente = Cerrado;
+                else if (!sense && obs)
+                    siguiente = Abriendo;
+                else if (sense && !obs)
+					siguiente = Cerrado;
+				else
+					siguiente = Abriendo;
+            end	
+			
+			Alarma: begin
+                if (!sense && !obs)
+                    siguiente = Abriendo;
+                else if (!sense && obs)
+                    siguiente = Alarma;
+                else if (sense && !obs)
+					siguiente = Abriendo;
+				else
+					siguiente = Alarma;
+            end
+
+            default: begin
+                siguiente = Cerrado;
+                alarm = 0;
+                motor = 2'b00;
+            end
+        endcase
+    end
+
+endmodule
